5e73f5e14ba3dae0924e8928e0873d5909f22ae6
Leo Riviera <leo.riviera@voxa.dev>
2023-12-14 06:33:12 +0000

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use rs_ws281x::ChannelBuilder;
+use rs_ws281x::ControllerBuilder;
+use rs_ws281x::StripType;
+
 mod colour;
 mod node;
 mod quantiser;
 
 use colorgrad::Color;
 use colour::Colour;
-use image::{GenericImageView, ImageBuffer, Rgb, RgbImage, Rgba};
+use image::{GenericImageView, Rgba};
 use quantiser::Quantiser;
-use std::fs;
 
 pub const MAX_DEPTH: u8 = 8;
 
@@ -20,7 +23,7 @@ fn pixel_to_colour(pixel: Rgba<u8>) -> Colour {
     }
 }
 
-fn generate_pixel_vector(path: &str, led_length: usize) -> Vec<Colour> {
+fn generate_pixel_vector(path: &str, led_length: usize) -> Vec<[u8; 4]> {
     let img = image::open(path).unwrap();
 
     let mut quantiser = Quantiser::default();
@@ -33,7 +36,7 @@ fn generate_pixel_vector(path: &str, led_length: usize) -> Vec<Colour> {
     }
 
     let colour_count = led_length / 2;
-    let palette = quantiser.make_palette(colour_count);
+    let palette = quantiser.make_palette(led_length * 2);
 
     let gradient = colorgrad::CustomGradient::new()
         .colors(
@@ -46,11 +49,11 @@ fn generate_pixel_vector(path: &str, led_length: usize) -> Vec<Colour> {
         .build()
         .unwrap();
 
-    let mut line: Vec<Colour> = Vec::new();
+    let mut line: Vec<[u8; 4]> = Vec::new();
 
     for point in (0..colour_count).map(|n| n as f64) {
         let gradient_point = gradient.at(point).to_rgba8();
-        line.push(Colour::from_rgba8(gradient_point));
+        line.push(gradient_point);
     }
 
     line.extend(line.clone().into_iter().rev());
@@ -59,28 +62,47 @@ fn generate_pixel_vector(path: &str, led_length: usize) -> Vec<Colour> {
 }
 
 fn main() {
-    let led_length = 300;
+    const PIN: i32 = 10;
+    const LED_LENGTH: usize = 300;
 
-    let paths = fs::read_dir("samples").unwrap();
+    let artwork_path = "samples/3CD66192B3012027F485E50EAEF5883EC9894B9FF8E3C89C61AA71286764B553_sk_12_cid_1.jpeg";
 
-    for path in paths {
-        let p = path.unwrap();
+    let mut controller = ControllerBuilder::new()
+        .freq(800_000)
+        .dma(10)
+        .channel(
+            0, // Channel Index
+            ChannelBuilder::new()
+                .pin(PIN) // GPIO 10 = SPI0 MOSI
+                .count(LED_LENGTH as i32) // Number of LEDs
+                .strip_type(StripType::Ws2812)
+                .brightness(10) // default: 255
+                .build(),
+        )
+        .build()
+        .unwrap();
 
-        let artwork_path = p.path().into_os_string().into_string().unwrap();
-        let file = p.file_name().into_string().unwrap();
+    let data: Vec<[u8; 4]> = generate_pixel_vector(artwork_path, LED_LENGTH);
 
-        let line = generate_pixel_vector(&artwork_path, led_length);
+    let leds = controller.leds_mut(0);
 
-        let mut img: RgbImage = ImageBuffer::new(led_length as u32, 1);
-
-        for (index, colour) in line.iter().enumerate() {
-            img.put_pixel(
-                index as u32,
-                0,
-                Rgb([colour.red as u8, colour.green as u8, colour.blue as u8]),
-            );
-        }
-
-        img.save(format!("output/{file}")).unwrap();
+    for (i, led) in leds.iter_mut().enumerate() {
+        let colour = data.get(i).unwrap();
+        dbg!(colour);
+        *led = *colour;
     }
+
+    controller.render().unwrap();
+
+    // let mut img: RgbImage = ImageBuffer::new(LED_LENGTH as u32, 1);
+
+    // for (index, colour) in line.iter().enumerate() {
+    //     img.put_pixel(
+    //         index as u32,
+    //         0,
+    //         Rgb([colour.red as u8, colour.green as u8, colour.blue as u8]),
+    //     );
+    // }
+
+    // img.save(format!("output/{file};")).unwrap();
 }
